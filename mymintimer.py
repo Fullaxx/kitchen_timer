d1784873eece3cf5b5122576d893297a661674fb
Brett Kuskie <fullaxx@gmail.com>
2024-05-22 11:42:41 +0000

    Update MinuteTimer to trigger on non-zero values, not just the top of the minute

--- mymintimer.py
+++ mymintimer.py
@@ -1,12 +1,13 @@
 
 
 class MinuteTimer:
-	def __init__(self, minutes: int=1):
+	def __init__(self, minutes: int=1, target: int=0):
 		self.last_trigger = 0
 		self.minutes = minutes
+		self.target = target
 
 	def check_for_ding(self, now: int) -> bool:
-		if ((now % (self.minutes*60)) == 0):
+		if ((now % (self.minutes*60)) == self.target):
 			if (now > self.last_trigger):
 				self.last_trigger = now
 				return True
